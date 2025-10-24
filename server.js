e56d589212df6f22163845e5f744dfcf6c02e08f
Aisha Umar <igotwebmail@gmail.com>
2025-10-24 13:32:15 -0400

    changed folder name

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const routes = require('./routes')
+const routes = require('../routes')
 const path = require("path")
 
 app.use(express.json())
