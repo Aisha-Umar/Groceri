d78e53a5200988610edd498d54da82a5b85ab7cd
Aisha Umar <igotwebmail@gmail.com>
2025-11-14 23:18:17 -0500

    added models for all schemas

--- models/item.js
+++ models/item.js
@@ -1,17 +1,26 @@
 const mongoose = require('mongoose');
 
-const grocerySchema = new mongoose.Schema({ 
-    item:{ 
-        type : String,
-        required : true,
-        trim: true 
-    },
-    order:{
-        type:Number,
-         default: 0, // so new items appear at the end if no order specified
-        index: true  // speeds up sorting queries
-}
-})
+const grocerySchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  item: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  order: {
+    type: Number,
+    default: 0, // so new items appear at the end if no order specified
+    index: true, // speeds up sorting queries
+  },
+  quantity: Number,
+  store: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Store",
+  },
+});
 const Grocery = mongoose.model('Grocery', grocerySchema)
 
 module.exports = Grocery
