40def600d8c9ee876a6d0d6c68eb6d4cf953d4c1
Aisha Umar <igotwebmail@gmail.com>
2025-12-10 16:52:29 -0500

    added middleware folder

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const loginController = require('../controllers/loginController')
 const signupController = require('../controllers/signupController')
-const { ensureAuthenticated, forwardAuthenticated } = require('../middlewares/auth');
+const { ensureAuthenticated, forwardAuthenticated } = require('../middleware/authMiddleware');
 
 router.get('/signup', forwardAuthenticated, signupController.getSignUp)
 router.get('/login', forwardAuthenticated, loginController.getLogin)
 router.post('/signup', signupController.signup)
 router.post('/login', loginController.login)
 router.get('/dashboard', ensureAuthenticated, controller.getDashboard)
+
 
 module.exports = router
