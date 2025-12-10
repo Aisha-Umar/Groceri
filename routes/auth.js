6c24a58d0742938b3446f435b3eb75beb708731a
Aisha Umar <igotwebmail@gmail.com>
2025-12-10 18:38:05 -0500

    required controllers into auth.js for dashboard

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,13 @@ const router = express.Router()
 const loginController = require('../controllers/loginController')
 const signupController = require('../controllers/signupController')
 const { ensureAuthenticated, forwardAuthenticated } = require('../middleware/authMiddleware');
+const controllers = require('../controllers/controllers.js')
 
 router.get('/signup', forwardAuthenticated, signupController.getSignUp)
 router.get('/login', forwardAuthenticated, loginController.getLogin)
 router.post('/signup', signupController.signup)
 router.post('/login', loginController.login)
-router.get('/dashboard', ensureAuthenticated, controller.getDashboard)
+router.get('/dashboard', ensureAuthenticated, controllers.getDashboard)
 
 
 module.exports = router
